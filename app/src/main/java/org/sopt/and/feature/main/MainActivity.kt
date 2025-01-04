7e76898471b20c59938e65388e4ed42b0377c20b
Sangho Kim <marchbreeze03@gmail.com>
2025-01-04 11:07:06 +0900

    [ADD/#2] MainScreen 설정

--- app/src/main/java/org/sopt/and/feature/main/MainActivity.kt
+++ app/src/main/java/org/sopt/and/feature/main/MainActivity.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.and.designsystem.theme.ANDANDROIDTheme
-import org.sopt.and.feature.login.LogInActivity.Companion.EXTRA_ID
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -21,30 +16,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navigator: MainNavigator = rememberMainNavigator()
+
             ANDANDROIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainRoute(
-                        name = intent.getStringExtra(EXTRA_ID).orEmpty(),
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(navigator = navigator)
             }
         }
-    }
-}
-
-@Composable
-fun MainRoute(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = if (name.isNotEmpty()) "$name 님 환영합니다!" else "로그인해주세요",
-        modifier = modifier.fillMaxSize()
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ANDANDROIDTheme {
-        MainRoute("Android")
     }
 }
---
+++ app/src/main/java/org/sopt/and/feature/main/MainNavigator.kt
@@ -0,0 +1,59 @@
+package org.sopt.and.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+
+class MainNavigator(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController.currentBackStackEntryAsState().value?.destination
+
+    val startDestination = LogIn(
+        email = "",
+        password = "",
+    )
+
+    val currentTab: MainTab?
+        @Composable get() = MainTab.find { route ->
+            currentDestination?.route == route.toString()
+        }
+
+    @Composable
+    fun showBottomBar(): Boolean = MainTab.contains { route ->
+        currentDestination?.route == route.toString()
+    }
+
+    fun navigate(tab: MainTab) {
+        val navOptions = navOptions {
+            navController.currentDestination?.route?.let {
+                popUpTo(it) {
+                    inclusive = true
+                    saveState = true
+                }
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+
+        when (tab) {
+            MainTab.HOME -> navController.navigateToHome(navOptions)
+            MainTab.SAVE -> navController.navigateToSave(navOptions)
+            MainTab.PROFILE -> navController.navigateToProfile(navOptions)
+        }
+    }
+
+    fun navigateUp() = navController.navigateUp()
+}
+
+@Composable
+internal fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController(),
+): MainNavigator = remember(navController) {
+    MainNavigator(navController)
+}
